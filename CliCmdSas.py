99b413b513fb40327915cb17e376aa3a2cd7564f
Larry Fenske <github@towanda.com>
2014-08-28 13:46:30 -0600

    Add expanderid to CliCmdSas.

--- CliCmdSas.py
+++ CliCmdSas.py
@@ -6,13 +6,21 @@ from CDB     import CDB
 
 class CliCmdSas(CliCmd):
     
-    def __init__(self, ptdev):
+    def __init__(self, ptdev, expanderid):
         """
         pt is a SCSI passthrough device file name
+        expanderid can have one of the values
+          0x01: SBB Canister A
+          0x02: SBB Canister B
+          0x03: FEM Canister A SAS Expander 1
+          0x04: FEM Canister A SAS Expander 2
+          0x05: FEM Canister B SAS Expander 1
+          0x06: FEM Canister B SAS Expander 2
         """
         super(CliCmdSas, self).__init__()
         self.ses = SesPageSas(ptdev)
         self.pt = ScsiPT(ptdev)
+        self.expanderid = expanderid
     
     def __del__(self):
         #del self.ses
@@ -22,7 +30,7 @@ class CliCmdSas(CliCmd):
         """
         Send a CLI command through SES page 0xe8.
         """
-        cmd = Cmd.clicommandout(1, command)
+        cmd = Cmd.clicommandout(self.expanderid, command)
         cdb = CDB(cmd.cdb)
         cdb.set_data_out(cmd.dat)
         self.pt.sendcdb(cdb)
