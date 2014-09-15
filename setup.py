6408d5f1acf0bb4c328b54c42a7cb71cb9014b84
Larry Fenske <github@towanda.com>
2014-09-15 16:27:03 -0600

    Add files for creating a package.     python setup.py sdist to create a distribution package under dist/.     pip install dist/wcdu-* to install the package in /usr/local/lib/python2.7/dist-packages/ and /usr/local/bin.     pip uninstall wcdu to remove this package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name='wcdu',
+    version='0.1.0a2',
+    py_modules=[
+        'configuration', 'discovery', 'Menu',
+        'CliCmd', 'CliCmdSerial', 'CliCmdTelnet', 'CliCmdSas',
+        'SesPage', 'SesPageFile', 'SesPageCli', 'SesPageSas',
+    ],
+    scripts=['wcdu', 'wcdu-cli', 'slowfan', 'slowfan2', 'slowfan3', ],
+    install_requires=['scsi_pt', ],
+)
