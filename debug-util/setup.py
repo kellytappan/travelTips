89b83060eead60a58f1f384313506e3c839f45b1
Larry Fenske <Larry_Fenske@jabil.com>
2014-10-08 10:53:40 -0600

    Add test for accessor in more places. Change version to 0.1.1a2.

--- debug-util/setup.py
+++ debug-util/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='wcdu',
-    version='0.1.1a1',
+    version='0.1.1a2',
     py_modules=[
         'configuration', 'discovery', 'Menu',
         'CliCmd', 'CliCmdSerial', 'CliCmdTelnet', 'CliCmdSas',
