ff637ca7149917b2e8846b0d7409638194b41823
Larry Fenske <Larry_Fenske@jabil.com>
2014-10-13 18:47:15 -0600

    Create and add wcdu-guide.

--- debug-util/setup.py
+++ debug-util/setup.py
@@ -1,12 +1,16 @@
 from distutils.core import setup
 setup(
     name='wcdu',
-    version='0.1.1a2',
+    version='0.1.1a3',
     py_modules=[
         'configuration', 'discovery', 'Menu',
         'CliCmd', 'CliCmdSerial', 'CliCmdTelnet', 'CliCmdSas',
         'SesPage', 'SesPageFile', 'SesPageCli', 'SesPageSas',
     ],
     scripts=['wcdu', 'wcdu-cli', 'slowfan3', 'discovery-test', ],
+    data_files=[('/usr/local/share/wcdu', ['wcdu-guide.pdf']), ],
     install_requires=['scsi_pt', 'argparse', 'pexpect', 'pyserial', ],
+    author='Larry Fenske',
+    author_email='jabil@towanda.com',
+    url='http://jabil.com',
 )
