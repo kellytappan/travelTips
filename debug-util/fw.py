a5dda17200c6b746acb0a02bb4ee3de161473336
Larry Fenske <Larry_Fenske@jabil.com>
2014-10-27 10:51:58 -0600

    Add firmwarefile.py to setup.py. Change version to 0.1.1a5. fw.py checkpoint.

--- debug-util/fw.py
+++ debug-util/fw.py
@@ -45,15 +45,21 @@ def display(scr):
     cans['PS2'] = {'win':curses.newwin(psh , modw, top, middle-modw)}
     cans['PS3'] = {'win':curses.newwin(psh , modw, top, middle+1   )}
     top += psh
-    cans['Ax' ] = {'win':curses.newwin(modh, modw, top, middle-modw)}
-    cans['Bx' ] = {'win':curses.newwin(modh, modw, top, middle+1   )}
+#     cans['Ax' ] = {'win':curses.newwin(modh, modw, top, middle-modw)}
+#     cans['Bx' ] = {'win':curses.newwin(modh, modw, top, middle+1   )}
+    cans['A0' ] = {'win':curses.newwin(modh, modw/2, top, middle-modw)}
+    cans['A1' ] = {'win':curses.newwin(modh, modw/2, top, middle-modw/2)}
+    cans['B0' ] = {'win':curses.newwin(modh, modw/2, top, middle+1   )}
+    cans['B1' ] = {'win':curses.newwin(modh, modw/2, top, middle+1+modw/2)}
     top += modh
     
-    cans['A' ]['plugpos'] = cans['B' ]['plugpos'] = ((1,23),)
-    cans['Ax']['plugpos'] = cans['Bx']['plugpos'] = ((4,modw/2-4),(4,modw/2))
-    cans['A' ]['verpos'] = ((1,1+iomw-modw),)
-    cans['B' ]['verpos'] = ((1,1),)
-    cans['Ax']['verpos'] = cans['Bx']['verpos'] = ((1,1),(1,modw/2))
+    cans['A' ]['plugpos'] = cans['B' ]['plugpos'] = (1,23)
+    cans['A0']['plugpos'] = cans['B0']['plugpos'] = (4,modw/2-4)
+    cans['A1']['plugpos'] = cans['B1']['plugpos'] = (4,1)
+    cans['A' ]['verpos'] = (1,1+iomw-modw)
+    cans['B' ]['verpos'] = (1,1)
+    cans['A0']['verpos'] = cans['B0']['verpos'] = (1,1)
+    cans['A1']['verpos'] = cans['B1']['verpos'] = (1,1)
 
     curses.init_pair(1, curses.COLOR_BLACK , curses.COLOR_RED   )
     curses.init_pair(2, curses.COLOR_BLACK , curses.COLOR_YELLOW)
@@ -64,10 +70,10 @@ def display(scr):
     curses.init_pair(7, curses.COLOR_GREEN , curses.COLOR_BLACK )
 
     for can in cans.values():
-        can['version'] = ["unknown"]*2
-        can['status' ] = ["unknown"]*2
-        can['tty'    ] = ["       "]*2
-        can['plug'   ] = [4        ]*2
+        can['version'] = "unknown"
+        can['status' ] = "unknown"
+        can['tty'    ] = "       "
+        can['plug'   ] = 4
     messages = ()
     bottom = top
 
@@ -84,16 +90,19 @@ def display(scr):
         for can in cans.values():
             can['win'].erase()
             can['win'].border()
+            for y in range(1,modh-1):
+                for name in ('A0','B0'):
+                    cans[name]['win'].addstr(y, modw/2-1, " ")
+                for name in ('A1','B1'):
+                    cans[name]['win'].addstr(y, 0, " ")
             if 'plugpos' in can:
-                for plugnum in range(len(can['plugpos'])):
-                    plug = can['plugpos'][plugnum]
-                    can['win'].addstr(plug[0], plug[1], " o ", curses.color_pair(can['plug'][plugnum]))
+                plug = can['plugpos']
+                can['win'].addstr(plug[0], plug[1], " o ", curses.color_pair(can['plug']))
             if 'verpos' in can:
-                for vernum in range(len(can['verpos'])):
-                    verpos = can['verpos'][vernum]
-                    can['win'].addstr(verpos[0]+0, verpos[1], "version: "+can['version'][vernum])
-                    can['win'].addstr(verpos[0]+1, verpos[1], "status: " +can['status' ][vernum])
-                    can['win'].addstr(verpos[0]+2, verpos[1], "tty: "    +can['tty'    ][vernum])
+                verpos = can['verpos']
+                can['win'].addstr(verpos[0]+0, verpos[1], "version: "+can['version'])
+                can['win'].addstr(verpos[0]+1, verpos[1], "status: " +can['status' ])
+                can['win'].addstr(verpos[0]+2, verpos[1], "tty: "    +can['tty'    ])
             can['win'].noutrefresh()
     
         curses.doupdate()
@@ -108,16 +117,20 @@ def display(scr):
 #                 break
         op = display_que.get()
         if   op[0] == CMD_SET:
-            cans[op[1]][op[2]][op[3]] = op[4]
+            # (CMD_SET, canister name, element name, new value)
+            cans[op[1]][op[2]] = op[3]
         elif op[0] == CMD_MESSAGE:
+            # (CMD_MSG, message)
             messages = op[1]
         elif op[0] == CMD_STOP:
+            # (CMD_STOP)
             return
         #curses.napms(1000)
 
 def watch_port(port):
     fw = FirmwareCli(port, None, verbosity)
     while cansleft:
+        #print port, cansleft, "\r"
         prompt = fw.get_prompt()
         if prompt:
             main_que.put((CMD_CONNECTED,port,prompt))
@@ -132,6 +145,8 @@ def watch_port(port):
             sleep(POLL_DELAY)
 
 
+connections = {}
+
 display_thr = threading.Thread(
     name='display',
     target=curses.wrapper,
@@ -143,13 +158,15 @@ display_thr.start()
 try:
     if False:
         sleep(1)
-        display_que.put((CMD_SET,'A' ,'plug',0,2))
+        display_que.put((CMD_SET,'A' ,'plug', 2))
         sleep(1)
-        display_que.put((CMD_SET,'Bx','plug',1,3))
+        display_que.put((CMD_SET,'B1','plug', 3))
+        sleep(1)
+        display_que.put((CMD_SET,'B0','plug', 1))
         sleep(1)
         display_que.put((CMD_MESSAGE,("Please plug in serial cables.",)))
         sleep(1)
-        display_que.put((CMD_SET, 'Ax', 'version', 0, '0278'))
+        display_que.put((CMD_SET, 'A0', 'version', '0278'))
         sleep(1)
         display_que.put((CMD_STOP,))
         sleep(1)
@@ -175,8 +192,21 @@ try:
         except Queue.Empty:
             continue
         if   op[0] == CMD_DISCONNECTED:
+            # (CMD_DISCONNECTED, port)
+            #print "CMD_DISCONNECTED", op[1], "\r"
+            if op[1] in connections:
+                display_que.put((CMD_SET, connections[op[1]], 'plug', 4))
+                del connections[op[1]]
             pass
         elif op[0] == CMD_CONNECTED:
+            # (CMD_CONNECTED, port, prompt)
+            #print "CMD_CONNECTED", op[1], op[2], "\r"
+            if op[1] in connections:
+                display_que.put((CMD_SET, connections[op[1]], 'plug', 4))
+                del connections[op[1]]
+            display_que.put((CMD_SET, op[2], 'plug', 1))
+            #print CMD_SET, op[2], 'plug', 1, "\r"
+            connections[op[1]] = op[2]
             pass
 
 except KeyboardInterrupt:
@@ -188,7 +218,7 @@ display_thr.join()
 if cansleft:
     print "The following canisters have not been programmed:"
     print " ".join(sorted(list(cansleft)))
-    cansleft = None
+    cansleft = ()
 for thr in port_threads:
     thr.join()
 
--- debug-util/setup.py
+++ debug-util/setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 setup(
     name='wcdu',
-    version='0.1.1a4',
+    version='0.1.1a5',
     py_modules=[
         'configuration', 'discovery', 'Menu',
         'CliCmd', 'CliCmdSerial', 'CliCmdTelnet', 'CliCmdSas',
         'SesPage', 'SesPageFile', 'SesPageCli', 'SesPageSas',
+	'firmwarefile', 
     ],
     scripts=['wcdu', 'wcdu-cli', 'slowfan3', 'discovery-test', 'firmwarecli.py', ],
     data_files=[('/usr/local/share/wcdu', ['wcdu-guide.pdf']), ],
