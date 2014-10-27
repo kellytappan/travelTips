1da8a287fff58fa47c4d43f8da7825b1948f96af
Larry Fenske <Larry_Fenske@jabil.com>
2014-10-27 10:42:42 -0600

    Add firmwarecli.py to setup.py and change version to 0.1.1a4.

--- debug-util/setup.py
+++ debug-util/setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
     name='wcdu',
-    version='0.1.1a3',
+    version='0.1.1a4',
     py_modules=[
         'configuration', 'discovery', 'Menu',
         'CliCmd', 'CliCmdSerial', 'CliCmdTelnet', 'CliCmdSas',
         'SesPage', 'SesPageFile', 'SesPageCli', 'SesPageSas',
     ],
-    scripts=['wcdu', 'wcdu-cli', 'slowfan3', 'discovery-test', ],
+    scripts=['wcdu', 'wcdu-cli', 'slowfan3', 'discovery-test', 'firmwarecli.py', ],
     data_files=[('/usr/local/share/wcdu', ['wcdu-guide.pdf']), ],
     install_requires=['scsi_pt', 'argparse', 'pexpect', 'pyserial', ],
     author='Larry Fenske',
