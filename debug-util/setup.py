5a1bc0991cdc6076bb5d2df0e5f5d5e5cd7491bb
Larry Fenske <Larry_Fenske@jabil.com>
2014-10-31 14:52:18 -0600

    Add descriptions from page 0x07 and column headings to page 0x05. Change version to 0.2.4.

--- debug-util/setup.py
+++ debug-util/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='wcdu',
-    version='0.2.3',
+    version='0.2.4',
     py_modules=[
         'configuration', 'discovery', 'Menu',
         'CliCmd', 'CliCmdSerial', 'CliCmdTelnet', 'CliCmdSas',
